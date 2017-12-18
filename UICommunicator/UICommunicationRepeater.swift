a570e0c5abef911899b9b71d89fb88118566b4e7
zevwings <zev.wings@gmail.com>
2017-12-18 22:42:12 +0800

    添加Bundle

--- UICommunicator/UICommunicationRepeater.swift
+++ UICommunicator/UICommunicationRepeater.swift
@@ -16,6 +16,9 @@ public protocol UICommunicationRepeater: UICommunicationProtocol {
     /// storyboard 对应 ViewController 的 storyboard id
     var identifier: String { get }
     
+    /// storyboard 的存储资源的的位置
+    var bundle: Bundle? { get }
+    
     /// 从发送器传递的参数
     var parameters: [String: Any]? { get }
     
@@ -27,7 +30,26 @@ public protocol UICommunicationRepeater: UICommunicationProtocol {
 
 public extension UICommunicationRepeater {
     
+    /// storyboard 名称
+    var storyboard: String { return "" }
+    
+    /// storyboard 对应 ViewController 的 storyboard id
+    var identifier: String { return "" }
+    
+    /// storyboard 的存储资源的的位置
+    var bundle: Bundle? { return nil }
+    
+    /// 从发送器传递的参数
+    var parameters: [String: Any]? { return nil }
+
+    
+    /// 从发送器转发参数到接收器， 只实现UIStoryboard 方式
+    ///
+    /// - Returns: 接收器(UICommunicationReceiver)
     func transmit() -> UICommunicationReceiver? {
+        
+        guard storyboard.count > 0 else { return nil }
+        guard identifier.count > 0 else { return nil }
         
         let sb = UIStoryboard(name: storyboard, bundle: nil)
         let vc = sb.instantiateViewController(withIdentifier: identifier) as? UICommunicationReceiver
